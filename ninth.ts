d410cc2ec847417ccff093f027d9f9803223df1c
jainChetanUnolo <chetan@unolo.com>
2022-12-21 16:20:07 +0530

    9th first part is done

--- ninth.ts
+++ ninth.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from "fs";
-const input: string[] = readFileSync("ninth-test.txt", "utf8").split("\n");
+const input: string[] = readFileSync("ninth-test-2.txt", "utf8").split("\n"); //6271
 // const input: string[] = readFileSync("ninth.txt", "utf8").split("\n");
 // this map will store the position of the tail
 // key will be the [x,y] position of the 2d Matrix
@@ -8,67 +8,108 @@ const tailPositionsHistory = new Map<string, number>();
 const headPositionsHistory = new Map<string, number>();
 // start the head position at 0,0, and then update the position based on the direction
 // the position can be negative as well
-let headPosition = [0, 0];
-let tailPosition= [0,0];
-for (let i = 0; i < input.length; i++) {
-	const [direction, position] = input[i].split(" ");
-	updatePosition(direction as "L" | "U" | "R" | "D", +position);
-}
-// console.log(isTailAwayFromHead());
+let headPosition: [number, number] = [0, 0];
+let tailPosition: [number, number] = [0, 0];
+//update the initial position of the tail and head
+tailPositionsHistory.set("0,0", 1);
+headPositionsHistory.set("0,0", 1);
+// for (let i = 0; i < input.length; i++) {
+// 	const [direction, position] = input[i].split(" ");
+// 	updatePosition(direction as "L" | "U" | "R" | "D", +position);
+// }
 
 function updatePosition(direction: "L" | "U" | "R" | "D", position: number) {
 	//update the position of the tail based on the direction of Head in the hashMap
 	for (let i = 0; i < position; i++) {
-		const [x, y] = headPosition;
+		// console.log("UPDATING POSITION", "position", i + 1, "of position", position, "of direction", direction);
+		const [headX, headY] = headPosition;
+		let tempHeadX = headX,
+			tempHeadY = headY;
 		switch (direction) {
 			case "L":
-				headPosition = [x - 1, y];
+				[tempHeadX, tempHeadY] = [tempHeadX - 1, tempHeadY];
 				break;
 			case "U":
-				headPosition = [x, y + 1];
+				[tempHeadX, tempHeadY] = [tempHeadX, tempHeadY + 1];
 				break;
 			case "R":
-				headPosition = [x + 1, y];
+				[tempHeadX, tempHeadY] = [tempHeadX + 1, tempHeadY];
 				break;
 			case "D":
-				headPosition = [x, y - 1];
+				[tempHeadX, tempHeadY] = [tempHeadX, tempHeadY - 1];
 				break;
 		}
-		const needToMoveTails = isTailAwayFromHead();
+		// update the head position
+		headPosition = [tempHeadX, tempHeadY];
+		const needToMoveTails = isTailAwayFromHead(headPosition, tailPosition);
 		if (needToMoveTails) {
-			//update latest position on the tail
-			const [x, y] = tailPosition;
-
+			// console.log("moving tail");
+			const [tailX, tailY] = tailPosition;
+			let tempTailX = tailX,
+				tempTailY = tailY;
+			// this is in case we should be moving diagonally
+			let moveDiagonal = false;
+			if (Math.abs(tempHeadX - tempTailX) >= 1 && Math.abs(tempHeadY - tempTailY) >= 1) {
+				// console.log(tempHeadX, tempHeadY, ": before, tail: ", tempTailX, tempTailY, "when moving diagonally:");
+				moveDiagonal = true;
+			}
 			switch (direction) {
 				case "L":
-					tailPosition = [x - 1, y];
+					[tempTailX, tempTailY] = moveDiagonal
+						? [tempTailX - 1, tempHeadY > tempTailY ? tempTailY + 1 : tempTailY - 1]
+						: [tempTailX - 1, tempTailY];
 					break;
 				case "U":
-					tailPosition = [x, y + 1];
+					[tempTailX, tempTailY] = moveDiagonal
+						? [tempHeadX > tempTailX ? tempTailX + 1 : tempTailX - 1, tempTailY + 1]
+						: [tempTailX, tempTailY + 1];
 					break;
 				case "R":
-					tailPosition = [x + 1, y];
+					[tempTailX, tempTailY] = moveDiagonal
+						? [tempTailX + 1, tempHeadY > tempTailY ? tempTailY + 1 : tempTailY - 1]
+						: [tempTailX + 1, tempTailY];
 					break;
 				case "D":
-					tailPosition = [x, y - 1];
+					[tempTailX, tempTailY] = moveDiagonal
+						? [tempHeadX > tempTailX ? tempTailX + 1 : tempTailX - 1, tempTailY - 1]
+						: [tempTailX, tempTailY - 1];
 					break;
 			}
-			console.log(headPosition, "tail position", tailPosition);
-			const tailKey = `${x},${y}`;
+			// update the tail position
+			tailPosition = [tempTailX, tempTailY];
+			const tailKey = `${tempTailX},${tempTailY}`;
 			const tailValue = tailPositionsHistory.get(tailKey) || 0;
 			tailPositionsHistory.set(tailKey, tailValue + 1);
 		}
+		// console.log(headPosition, ": head, tail: ", tailPosition);
+
 		// update the position of the head in the hashMap
-		const key = `${x},${y}`;
+		const key = `${tempHeadX},${tempHeadY}`;
 		const value = headPositionsHistory.get(key) || 0;
 		headPositionsHistory.set(key, value + 1);
+		// console.log("END UPDATING POSITION");
 	}
 }
-
-function isTailAwayFromHead() {
+console.log("tailPositionsHistory.size()", tailPositionsHistory.size);
+function isTailAwayFromHead(head: [number, number], tail: [number, number]) {
 	//check if the distance between the head and tail is greater than 1
 	const [x1, y1] = headPosition;
 	const [x2, y2] = tailPosition;
 	if (Math.abs(x1 - x2) <= 1 && Math.abs(y1 - y2) <= 1) return false;
 	return true;
+}
+const positions = {
+	1: [0, 0],
+	2: [0, 0],
+	3: [0, 0],
+	4: [0, 0],
+	5: [0, 0],
+	6: [0, 0],
+	7: [0, 0],
+	8: [0, 0],
+	9: [0, 0],
+};
+for (let i = 0; i < input.length; i++) {
+	const [direction, position] = input[i].split(" ");
+	updatePosition(direction as "L" | "U" | "R" | "D", +position);
 }
